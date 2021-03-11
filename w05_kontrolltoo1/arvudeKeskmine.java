a2fab132dbcb65ed65239aed569540d470452f10
Pille Allvee <70939481+pilleae@users.noreply.github.com>
2021-03-11 21:21:57 +0200

    proovi KT 1/3 aritm keskmine

--- w05_kontrolltoo1/arvudeKeskmine.java
+++ w05_kontrolltoo1/arvudeKeskmine.java
@@ -1,9 +1,9 @@
 public class arvudeKeskmine {
     public static void main(String[] args){
         int[] num={102, 34, 22};
-        float count=0;
-        float sum=0;
-        float avg=0;
+        int count=0;
+        int sum=0;
+        int avg=0;
         for(int i=0; i<3; i++){
             sum = sum + num[i];
             count = count +1;
@@ -11,6 +11,6 @@ public class arvudeKeskmine {
         
         }
         
-        System.out.println("Aritmeetiline keskmine: " + avg);
+        System.out.println("Arvude ymardatud aritmeetiline keskmine: " + avg);
     }
 }
